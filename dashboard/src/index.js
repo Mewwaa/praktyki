ac816bc390c570d8475dec2dfdb7761debd6003e
Ewa Zalewska <zalewska.ewa.2004@gmail.com>
2022-04-20 13:56:21 +0200

    removed unnecessary files

--- dashboard/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Dashboard from './dashboard';
-// import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(<Dashboard />, document.getElementById('root'));
-
-// serviceWorker.unregister();
