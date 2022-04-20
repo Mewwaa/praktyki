2017350f27574d7ee2ec95bd7b2518d6deab29a0
Michał Sobieszczyk <michal.sobieszczyk@tomtom.com>
2022-04-20 13:24:55 +0200

    wip

--- react-hello-world-master/src/login.js
+++ react-hello-world-master/src/login.js
@@ -1,15 +1,22 @@
-function validate()
-{
-    var username=document.getElementById("username").value;
-    var password=document.getElementById("password").value;
-    if(username=="tomtom@tomtom.com"&& password=="tomtom")
-    {
-        document.getElementById("alert").hidden = true;
-        return false;
-    }
-    else
-    {
+import React, { Component } from 'react';
+import './login.css';
+import logo from './logo.jpg'
 
-        document.getElementById("alert").hidden = false; 
-    }
+class Login extends Component {
+
+  render() {
+    return (
+      <div className="Login">
+        <header className="Login-header">
+        <img src={logo} alt="logo"/>
+        <form className="box" method="POST">
+            <p><h2>Welcome in our app</h2></p>
+            
+        </form>
+
+        </header>
+      </div>
+    );
+  }
 }
+export default Login;
