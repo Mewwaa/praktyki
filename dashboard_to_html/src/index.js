4cc7d3bd744d5fd8f8c54f6615fb9635230e07c8
Ewa Zalewska <zalewska.ewa.2004@gmail.com>
2022-04-20 12:20:16 +0200

    Dashboard to html / Browse slack channels to html

---
+++ dashboard_to_html/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Dashboard from './dashboard';
+// import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(<Dashboard />, document.getElementById('root'));
+
+// serviceWorker.unregister();
