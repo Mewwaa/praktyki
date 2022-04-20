a55ce2e9a82420f1a3bb4cbe214dc0ca8f997662
Michał Sobieszczyk <michal.sobieszczyk@tomtom.com>
2022-04-20 13:08:09 +0200

    fixes

--- react-hello-world-master/src/App.js
+++ react-hello-world-master/src/App.js
@@ -1,28 +1,55 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+const express = require('express');
+const app = express();
+const port = 3000;
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hello world!
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+// Display the Sign in with Slack button
+
+app.get('/sign-in', async (_, res) => {
+  // TODO: adding one-time state parameter as well
+  const scopes = "identity.basic,identity.email"; // don't mix other non-`identity.` scopes here
+  // if you already have another Redirect URL for "Add to Slack", you may need to pass redirect_uri as well
+  const clientId = '3372401797858.3408464854688'
+  const url = `https://slack.com/oauth/v2/authorize?user_scope=${scopes}&client_id=${clientId}`;
+
+  res.status(200)
+    .header('Content-Type', 'text/html; charset=utf-8')
+    // https://api.slack.com/docs/sign-in-with-slack#generator
+    .send(`<html>
+  <body>
+  <a href="${url}"><img alt="Sign in with Slack" height="40" width="172" src="https://platform.slack-edge.com/img/sign_in_with_slack@2x.png"/></a>
+  </body>
+  </html>
+  `);
+});
+
+
+// Verify the installation
+const { WebClient } = require('@slack/web-api');
+const client = new WebClient();
+
+app.get('/sign-in-callback', async (req, res) => {
+  // TODO: verify state parameter
+  try {
+    const response = await client.oauth.v2.access({
+      client_id: process.env.SLACK_CLIENT_ID,
+      client_secret: process.env.SLACK_CLIENT_SECRET,
+      code: req.query.code,
+    });
+    console.log(response);
+
+    const identity = await client.users.identity({
+      token: response.authed_user.access_token
+    });
+    console.log(identity);
+    // Now you can assume this user logged in with his/her Slack account
+    // TODO: set-cookie etc
+    res.status(200).send("You've logged in with your Slack account!");
+  } catch (e) {
+    console.log(e);
+    res.status(500).send("Something wrong!");
   }
-}
+});
 
-export default App;
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}/sign-in`)
+});
