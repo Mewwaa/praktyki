621681a345d28d72313350fe6881c2d43f0fa975
Ewa Zalewska <79149134+Mewwaa@users.noreply.github.com>
2022-04-21 11:08:27 +0200

    Update main.js

--- react-hello-world-master/src/main.js
+++ react-hello-world-master/src/main.js
@@ -2,8 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './main.css';
 import Dashboard from './dashboard';
-// import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<Dashboard />, document.getElementById('root'));
 
-// serviceWorker.unregister();
+
