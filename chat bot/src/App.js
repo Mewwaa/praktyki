2b0e82c437a601ff4ee5f5f7ffa8b5ca37ae4ef3
Damian Galiński <damian.galinski@tomtom.com>
2022-04-21 13:23:56 +0200

    matynia-galinski/chat-to-jsx-html

---
+++ chat bot/src/App.js
@@ -0,0 +1,25 @@
+import { ReactSlackChat } from "react-slack-chat/dist/react-slack-chat-with-default-hooks";
+import React from "react";
+
+export default function App() {
+  return (
+    <ReactSlackChat
+      botName=""
+      apiToken=""
+      channels={[
+        {
+          name: "random",
+          id: "",
+          icon: "./logo.svg"
+        },
+        {
+          name: "general",
+          id: ""
+        }
+      ]}
+      helpText="Chat"
+      themeColor="#856090"
+      userImage="http://www.iconshock.com/img_vista/FLAT/mail/jpg/robot_icon.jpg"
+    />
+  );
+}
