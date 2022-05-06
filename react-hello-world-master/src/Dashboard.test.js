07e54b830153536b8cc69096176529225ca6b5e4
Bartłomiej Gędek <bartlomiej.gedek@tomtom.com>
2022-05-06 11:41:47 +0200

    gedek/tests

--- react-hello-world-master/src/Dashboard.test.js
+++ react-hello-world-master/src/Dashboard.test.js
@@ -19,34 +19,6 @@ var message4 = new messages("FAILED","Random message 4")
 
 var messagesList = [ message1, message2, message3, message4]
 
-class messages{
-  constructor(ifSucceded,content){
-    this.ifSucceded = ifSucceded
-    this.content = content
-  }
-}
-
-class channels {
-  constructor(name){
-    this.name = name;
-  }
-}
-
-
-var channels1 = new channels("channel 1")
-var channels2 = new channels("channel 2")
-var channels3 = new channels("channel 3")
-var channels4 = new channels("channel 4")
-
-var channelsList=[channels1, channels2, channels3, channels4]
-
-
-var message1 = new messages("FAILED","Random message 1")
-var message2 = new messages("SUCCEDED","Random message 2")
-var message3 = new messages("SUCCEDED","Random message 3")
-var message4 = new messages("FAILED","Random message 4")
-
-var messagesList = [ message1, message2, message3, message4]
 
 it('renders without crashing', () => {
   localStorage.setItem('channels', JSON.stringify(channelsList));
@@ -54,6 +26,6 @@ it('renders without crashing', () => {
   localStorage.setItem('clientId', '3372401797858.3408464854688');
   
   const div = document.createElement('div');
-  ReactDOM.render(<Dashboard  store1={channelsList} store2={messagesList}/>, div);
+  ReactDOM.render(<Dashboard channels={channelsList} messages={messagesList}/>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
