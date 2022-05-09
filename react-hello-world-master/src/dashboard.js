fceead70bca250d919bb2561b582f62f7846e357
Ewa Zalewska <zalewska.ewa.2004@gmail.com>
2022-05-09 11:46:04 +0200

    new token for bot

--- react-hello-world-master/src/dashboard.js
+++ react-hello-world-master/src/dashboard.js
@@ -26,7 +26,7 @@ function browseChannels(text) {
 }
 
 const { WebClient }  = require('@slack/web-api');
-const token = 'xoxb-3372401797858-3387082004324-nwwMUGrm6j2iz5WUCvCC8wUt'
+const token = 'xoxb-3372401797858-3387082004324-G2bdhMxnftd8hnFFPNlLMPQ9'
 const web = new WebClient(token);
 delete web["axios"].defaults.headers["User-Agent"];
 
